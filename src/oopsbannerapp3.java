adcf642253d4b6da0ca47f4a80a27e58ea4b2b1b
dingdongditch123 <thisisanemailadressbro99@gmail.com>
2026-03-12 16:54:44 +0530

    UC3 implemented: OOPS banner using String.join()

---
+++ src/oopsbannerapp3.java
@@ -0,0 +1,42 @@
+/**
+ * OOPS Banner App
+ * UC3: Using String.join() to build the banner lines
+ *
+ * @author Satwik
+ * @version 1.2
+ */
+
+class oopsbannerapp3 {
+
+    public static void main(String[] args) {
+
+        String line1 = String.join(" ",
+                "*****", "*****", "*****", "*****");
+
+        String line2 = String.join(" ",
+                "*   *", "*   *", "*   *", "*   *");
+
+        String line3 = String.join(" ",
+                "*   *", "*   *", "*   *", "*   *");
+
+        String line4 = String.join(" ",
+                "*   *", "*****", "*****", "*****");
+
+        String line5 = String.join(" ",
+                "*   *", "*", "*", "*");
+
+        String line6 = String.join(" ",
+                "*   *", "*", "*", "*");
+
+        String line7 = String.join(" ",
+                "*****", "*", "*", "*****");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
