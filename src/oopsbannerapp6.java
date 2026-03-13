404f9f88b530ddf25cde2ef1ef7f77753678afab
dingdongditch123 <thisisanemailadressbro99@gmail.com>
2026-03-13 10:16:14 +0530

    UC6 implemented: banner using array and loop

---
+++ src/oopsbannerapp6.java
@@ -0,0 +1,19 @@
+public class oopsbannerapp6 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            " *****   *****   *****   ***** ",
+            "*     * *     * *     *  *     ",
+            "*     * *     * *     *  *      ",
+            "*     * *     * *****    *****  ",
+            "*     * *     * *            *  ",
+            "*     * *     * *            *  ",
+            " *****   *****  *        *****   "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
